2ca38d38766164fa17909ddf99908036b75197a9
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-11-28 18:10:40 -0800

    task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Install nginx web server - w/puppet
+# perform a 301 redirect when querying /redirect_me
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
