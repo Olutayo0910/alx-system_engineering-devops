ae76c88917ba709884f21d3627f46b1b4718c87e
Olutayo0910 <olutayoogunlade0910@gmail.com>
2024-01-04 22:09:01 -0800

    done

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Exports data in JSON format"""
+
+if __name__ == "__main__":
+
+    import json
+    import requests
+    import sys
+
+    user = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = user.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
+
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {
+                        "username": user.get('username'),
+                        "task": task.get('title'),
+                        "completed": task.get('completed')
+                        }
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
